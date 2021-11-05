4957efdc3f1bb1ef29a56da7d82de6fe760b882c
Park-HY-00 <phy000115@naver.com>
2021-11-05 14:00:05 +0900

    Modified code, Upload text file

--- tree.c
+++ tree.c
@@ -33,7 +33,6 @@ void setBinNode(BinNode *node, int data, BinNode *left_child, BinNode *right_chi
     node->right = right_child;
 }
 
-int count = 0; // addNode 함수에서 사용할 제어변수 count 전역변수로 선언
 /* node 추가 함수 */
 BinNode *addNode(BinNode *node, int data)
 {
@@ -42,7 +41,6 @@ BinNode *addNode(BinNode *node, int data)
         // node가 비어있는 경우, data를 node에 추가
         node = getBinaryNode();
         setBinNode(node, data, NULL, NULL);
-        // printf("node 생성완료 (%2d), counter : %d\n", node->data, count++);
     }
     else if (data < node->data)
     {
@@ -57,6 +55,7 @@ BinNode *addNode(BinNode *node, int data)
     return node;
 }
 
+/* 정렬된 node 출력 함수 */
 void showTree(const BinNode *node)
 {
     if (node != NULL)
@@ -73,13 +72,13 @@ BinNode *searchTree(BinNode *ptr, int key)
     if (ptr == NULL)
     {
         // 찾는 key 값이 tree에 없음
-        printf("FAIL\n");
+        printf("\nFAIL to search %d\n", key);
         return NULL;
     }
     else if (key == ptr->data)
     {
         // 찾는 key 값이 tree 내에 존재함
-        printf("SUCCESS\n");
+        printf("\nSUCCESS to search %d\n", key);
     }
     else if (key < ptr->data)
     {
@@ -107,46 +106,49 @@ void deleteTree(BinNode *node)
 int main(void)
 {
     // Show the message that the program start
-    printf("Start the program\n");
+    printf("\nStart the program\n");
 
     // Text file open as read mode
     FILE *pFile = fopen("test.txt", "r");
     printf("Open the file as read mode\n");
 
     int intTemp = -1;
-    int key;
-    BinNode *root = NULL;
-    BinNode *search = NULL;
+    int key;                // 탐색할 key 값
+    BinNode *root = NULL;   // 값 저장하여 정렬할 tree node 선언
+    BinNode *search = NULL; // key 탐삭할 node 선언
 
     if (pFile == NULL)
     {
+        // file open 실패 시, 프로그램 종료
         printf("File does not exist\n");
         return 0;
     }
 
-    // 파일 읽기 성공
+    // file open 성공 시
     while (pFile != NULL)
     {
         fscanf(pFile, "%d", &intTemp);
         if (intTemp < 0)
-            break;
+            break; // file의 끝을 알리는 '-1' 값에 도달하면 이 program unit 종료
+
         // 트리생성
         root = addNode(root, intTemp);
     }
-    printf("\n/*-----정렬 성공-----*/\n");
+
+    printf("\n/*-----Sorting Success-----*/\n"); // 정렬에 성공했음을 알림
+
     // 생성된 tree 보기
     // showTree(root);
 
     // 생성된 트리에서 키 검색
-    printf("\n검색할 key 값 입력 : ");
-    // scanf("%d", &key);
+    printf("\nEnter the key value to search : ");
+    scanf("%d", &key);
+    search = searchTree(root, key);
 
-    // printf("%d \n", intTemp);
+    // 파일 형태로 반환
+    fclose(pFile); // close file
 
-    //파일 형태로 반환
+    deleteTree(root); // tree에 할당된 node 해제
 
-    fclose(pFile); //close file
-
-    deleteTree(root);
     return 0;
 }
